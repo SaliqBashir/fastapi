8b1f8078e3aa327f5b639c5b9717a401e6dbd9ca
SaliqBashir <salikbhat541@gmail.com>
2026-06-04 14:42:29 +0530

    fixed some things

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
 from google import genai
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 app = FastAPI()
+
+
+items = []
 
 
 @app.get("/")
@@ -13,9 +18,15 @@ def root():
 
 
 @app.post("/message")
-def getResponse(message: str) -> str:
+def get_response(message: str) -> str:
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=message,
     )
     return response.text
+
+
+@app.get("/items")
+def item(item: str) -> list[str]:
+    items.append(item)
+    return items
