0415651d4d3eea86d154537bc880f9304b7d332c
SaliqBashir <salikbhat541@gmail.com>
2026-06-06 19:27:48 +0530

    tested things with pydantic

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from google import genai
 import os
 from dotenv import load_dotenv
+from pydantic import BaseModel
 
 load_dotenv()
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+
+class List(BaseModel):
+    text: str = None
+    is_done: bool = False
+
 
 app = FastAPI()
 
 
 items = []
+lists = []
 
 
 @app.get("/")
@@ -26,7 +34,40 @@ def get_response(message: str) -> str:
     return response.text
 
 
-@app.get("/items")
-def item(item: str) -> list[str]:
+@app.post("/items")
+def create_item(item: str):
     items.append(item)
-    return items
+    return
+
+
+@app.get("/items")
+def get_item_list(limit: int = 10) -> list[str]:
+    return items[0:limit]
+
+
+@app.get("/items/{item_id}")
+def get_item(item_id: int) -> str:
+    if item_id < len(items):
+        return items[item_id]
+    else:
+        raise HTTPException(status_code=404, detail="Invalid item id")
+
+
+@app.post("/add_item")
+def todo(item: List):
+    lists.append(item)
+
+
+@app.get("/get_item")
+def todo_get(num: int = 10):
+    return lists[0:num]
+
+
+
+
+
+
+
+
+
+
