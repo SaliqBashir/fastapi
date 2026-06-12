ac0ff288c4aea3bc552d26a127124b7571ed030f
SaliqBashir <salikbhat541@gmail.com>
2026-06-12 15:42:38 +0530

    updated gemAI.py

--- gemAI.py
+++ gemAI.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from google import genai
 import os
 from dotenv import load_dotenv
+from fastapi.responses import StreamingResponse
 
 load_dotenv()
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
@@ -10,10 +11,24 @@ client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 app = FastAPI()
 
 
-@app.post("/message")
+@app.post("/response")
 def get_response(message: str) -> str:
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=message,
     )
     return response.text
+
+
+@app.post("/stream")
+def get_response_stream(message: str) -> str:
+    response = client.models.generate_content_stream(
+        model="gemini-2.5-flash",
+        contents=message,
+    )
+
+    def generate():
+        for stream in response:
+            if stream.text:
+                yield stream.text
+    return StreamingResponse(generate(), media_type="text/plain")
